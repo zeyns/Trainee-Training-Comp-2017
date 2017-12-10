b41cc66886b7c6849e7337f2839b8a7ac36983d0
Marcao <marcoslucsrios@gmail.com>
2017-12-10 06:53:57 -0200

    Adicionando parte final

---
+++ Part 3 - Web Performace/assets/js/indexJQuery.js
@@ -0,0 +1,25 @@
+
+/* Usando JQUERY */
+
+/* When a click occurs on the tag with btnNewCard ID */
+$("#btnNewCard").click(function()
+{
+	/* Change the display of the tag with btnNewCard ID to none */
+	$(this).hide();
+	/* Enter an html code in the tag with ID listCards */
+	$("#listCards").append(
+		"<li class=\"col-sm-12 col-md-3 col-l-3 col-xl-3\">"
+			+"<div class=\"card\">"
+				+"<img src=\"assets/media/img/pokemon.jpg\" class=\"img-card\">"
+				+"<p>Pokemon</p>"
+			+"</div>"
+		+"</li>"
+	)
+});
+
+/* When a click occurs in the DOM on a daughter tag of a tag with ID listCards */
+$("#listCards>li").click(function()
+{
+	/* Get this tag li and disappear with fade effect */
+	$(this).fadeOut(600);
+})
